f34e1084dc0273372afb40b0fe94f77509252b00
Vadim Potapov <doctorvra4@gmail.com>
2016-03-30 00:21:51 +0600

    add_Lion_class

---
+++ Main.py
@@ -0,0 +1,37 @@
+class Lion(object):
+    def __init__(self, event, condition):
+        self.condition = condition
+        self.event = event
+        self.action = ""
+        self.isHungry()
+
+    def isHungry(self):
+        if self.condition == "Голодный":
+            self.Hungry()
+        elif self.condition == "Сытый":
+            self.notHungry()
+        else:
+            raise ValueError("Неверный параметр Сытый / Голодный")
+
+    def Hungry(self):
+        if self.event == "Антилопа":
+            self.action = "Съесть"
+            self.condition = "Сытый"
+        elif self.event == "Охотник":
+            self.action = "Бежать"
+        elif self.event == "Дерево":
+            self.action = "Спать"
+        else:
+            raise ValueError("Неверный параметр события")
+
+    def notHungry(self):
+        self.condition = "Голодный"
+        if self.event == "Антилопа":
+            self.action = "Спать"
+        elif self.event == "Охотник":
+            self.action = "Бежать"
+        elif self.event == "Дерево":
+            self.action = "Смотреть"
+        else:
+            raise ValueError("Неверный параметр события")
+
