be1f1346977026af61b6ec5ac5b5d32c3fecddf2
Vadim Potapov <doctorvra4@gmail.com>
2016-03-30 10:18:08 +0600

    add_Test

--- unitest.py
+++ unitest.py
@@ -1,16 +1,32 @@
 from Main import Lion
 import unittest
 
+
 class LionUniTest(unittest.TestCase):
-    def test_LionTestIO (self):
-        res = Lion("Дерево","Голодный")
+    def test_LionTestIO(self):
+        res = Lion("Дерево", "Голодный")
         self.assertEqual("Спать", res.action, "False")
-        self.assertEqual("Голодный",res.condition,"Error")
+        self.assertEqual("Голодный", res.condition, "False")
 
         res = Lion("Дерево", "Сытый")
         self.assertEqual("Смотреть", res.action, "False")
-        self.assertEqual("Голодный", res.condition, "Error")
+        self.assertEqual("Голодный", res.condition, "False")
 
+        res = Lion("Охотник", "Голодный")
+        self.assertEqual("Бежать", res.action, "False")
+        self.assertEqual("Голодный", res.condition, "False")
+
+        res = Lion("Охотник", "Сытый")
+        self.assertEqual("Бежать", res.action, "False")
+        self.assertEqual("Голодный", res.condition, "False")
+
+        res = Lion("Антилопа", "Голодный")
+        self.assertEqual("Съесть", res.action, "False")
+        self.assertEqual("Сытый", res.condition, "False")
+
+        res = Lion("Антилопа", "Сытый")
+        self.assertEqual("Спать", res.action, "False")
+        self.assertEqual("Голодный", res.condition, "False")
 
 
 if __name__ == '__main__':
