47d6b3c5136e980c75476cfee7c841d3c92bd1fc
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-04 16:57:21 +0600

    Update LionSateFMS_Test.py
    Разбила тест FMS на более мелкие
    Добавила проверку исходных состояний: test_init_fed, test_init_hungry
    Добавила проверку работы FMS для всех состояний в цикле

--- LionSateFMS_Test.py
+++ LionSateFMS_Test.py
@@ -2,48 +2,68 @@ from unittest import TestCase
 from LionStFMS import LionStateFSM
 import unittest
 
-class LionSateFMS_Test(TestCase):
 
-    #Тест вывода ошибки конструктором, при вводе не верных значений символа и состояния
+class LionSateFMSTest(TestCase):
+
+    # Тест вывода ошибки конструктором, при вводе не верного значения состояния
     def test_init(self):
-        self.assertRaises(NameError, LionStateFSM, "фывваыв", "sdfaf")
+        self.assertRaises(NameError, LionStateFSM, "jhlkhlk")
 
-    #Тест корректности работы конечного автомата состояния
-    def test_fms(self):
-        lion = LionStateFSM("антилопа", "сытый")
-        lion.fsm_realisation()
+    # Тест вывода ошибки конструктором, при ошибке исходного состояния голодный
+    def test_init_hungry(self):
+        lion = LionStateFSM("голодный")
+        self.assertEqual("голодный", lion.state, "Ошибка! Должен быть голодный")
+
+    # Тест вывода ошибки конструктором, при ошибке исходного состояния сытый
+    def test_init_fed(self):
+        lion = LionStateFSM("сытый")
+        self.assertEqual("сытый", lion.state, "Ошибка! Должен быть сытый")
+
+    # Тест корректности работы конечного автомата состояния при сытом льве и объекте антилопа
+    def test_fms_fed_antelope(self):
+        lion = LionStateFSM("сытый")
+        lion.fsm_realisation("антилопа")
         self.assertEqual("спать", lion.action, "Ошибка! Должен быть спать")
         self.assertEqual("голодный", lion.state, "Ошибка! Должен быть голодный")
 
-        lion = LionStateFSM("антилопа", "голодный")
-        lion.fsm_realisation()
+    # Тест корректности работы конечного автомата состояния при голодном льве и объекте антилопа
+    def test_fms_hungry_antelope(self):
+        lion = LionStateFSM("голодный")
+        lion.fsm_realisation("антилопа")
         self.assertEqual("съесть", lion.action, "Ошибка! Должен быть съесть")
-        self.assertEqual("сытый", lion.state, "Ошибка! Должен быть сытый")
 
-        lion = LionStateFSM("охотник", "сытый")
-        lion.fsm_realisation()
+    # Тест корректности работы конечного автомата состояния при сытом льве и объекте охотник
+    def test_fms_fed_hunter(self):
+        lion = LionStateFSM("сытый")
+        lion.fsm_realisation("охотник")
         self.assertEqual("убежать", lion.action, "Ошибка! Должен быть убежать")
         self.assertEqual("голодный", lion.state, "Ошибка! Должен быть голодный")
 
-        lion = LionStateFSM("охотник", "голодный")
-        lion.fsm_realisation()
+    # Тест корректности работы конечного автомата состояния при голодном льве и объекте охотник
+    def test_fms_hungry_hunter(self):
+        lion = LionStateFSM("голодный")
+        lion.fsm_realisation("охотник")
         self.assertEqual("убежать", lion.action, "Ошибка! Должен быть убежать")
         self.assertEqual("голодный", lion.state, "Ошибка! Должен быть голодный")
 
-        lion = LionStateFSM("дерево", "сытый")
-        lion.fsm_realisation()
+    # Тест корректности работы конечного автомата состояния при сытом льве и объекте дерево
+    def test_fms_fed_tree(self):
+        lion = LionStateFSM("сытый")
+        lion.fsm_realisation("дерево")
         self.assertEqual("смотреть", lion.action, "Ошибка! Должен быть убежать")
         self.assertEqual("голодный", lion.state, "Ошибка! Должен быть голодный")
 
-        lion = LionStateFSM("дерево", "голодный")
-        lion.fsm_realisation()
+    # Тест корректности работы конечного автомата состояния при сытом льве и объекте дерево
+    def test_fms_hungry_tree(self):
+        lion = LionStateFSM("голодный")
+        lion.fsm_realisation("дерево")
         self.assertEqual("спать", lion.action, "Ошибка! Должен быть спать")
         self.assertEqual("голодный", lion.state, "Ошибка! Должен быть голодный")
 
+    # Тест корректности работы конечного автомата
+    def test_fms(self):
+        lion = LionStateFSM("сытый")
+        lion.lion_fms_work()
+
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
