6c3c06640266e85409cb4137d259d2a3a6d193d4
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-04 16:37:39 +0600

    Update LionStFMS.py
    Изменила код конструктора
    Добавила метод lion_fms_work(self)

--- LionStFMS.py
+++ LionStFMS.py
@@ -1,37 +1,43 @@
 
 class LionStateFSM:
+    objects_list = ["антилопа", "охотник", "дерево"]
 
-    def __init__(self, obj, state):
-        if obj == "антилопа" or obj == "охотник" or obj == "дерево":
-            self.obj = obj.lower()
-        else:
-            raise NameError("Нет такого объекта")
+    # Инициализируем льва с определенным состоянием
+    def __init__(self, state):
         if state == "сытый" or state == "голодный":
             self.state = state.lower()
         else:
             raise NameError("Нет такого состояния")
         self.action = ""
 
-    def fsm_realisation(self):
+    # Реализация конечного автомата состояния льва
+    def fsm_realisation(self, obj):
         print("Исходное состояние :" + self.state)
-        if self.obj == "антилопа":
+        if obj == "антилопа":
             if self.state == "сытый":
                 self.action = "спать"
                 self.state = "голодный"
             else:
                 self.action = "съесть"
                 self.state = "сытый"
-        if self.obj == "охотник":
+        if obj == "охотник":
             if self.state == "сытый":
                 self.action = "убежать"
                 self.state = "голодный"
             else:
                 self.action = "убежать"
-
-        if self.obj == "дерево":
+        if obj == "дерево":
             if self.state == "сытый":
                 self.action = "смотреть"
                 self.state = "голодный"
             else:
                 self.action = "спать"
-        print("Объект: " + self.obj + "| Действие: " + self.action + "| Новое_Состояние :" + self.state)
+        print("Объект: " + obj + "| Действие: " + self.action + "| Новое_Состояние :" + self.state)
+
+    # Запуск FMS для всех возможных объектов в нескольких циклах
+    def lion_fms_work(self):
+        n = 0
+        while n < 3:
+            for obj in self.objects_list:
+                self.fsm_realisation(obj)
+            n += 1
