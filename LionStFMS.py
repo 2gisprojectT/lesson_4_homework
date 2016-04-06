2f1d4c33b6fa7b58e92395de22371a4137df6afe
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-06 22:07:27 +0600

    Update LionStFMS.py
    Изменения:
    1.Убрала комментарии
    2.Изменила список на словарь в конструкторе
    3.Изменила логику FMS

--- LionStFMS.py
+++ LionStFMS.py
@@ -1,34 +1,33 @@
 
 class LionStateFSM:
 
-    # Инициализируем льва с определенным состоянием
     def __init__(self, state):
         if state == "сытый" or state == "голодный":
             self.state = state
         else:
             raise ValueError("Нет такого состояния: ", state)
-        self.state_list = [
-                            ['антилопа', 'сытый', 'спать', 'голодный'],
-                            ['антилопа', 'голодный', 'съесть', 'сытый'],
-                            ['охотник', 'сытый', 'убежать', 'голодный'],
-                            ['охотник', 'голодный', 'убежать', 'голодный'],
-                            ['дерево', 'сытый', 'смотреть', 'голодный'],
-                            ['дерево', 'голодный', 'спать', 'голодный']
-                         ]
+        self.state_list = {
+                            'сытый': {
+                                'антилопа': ['спать', 'голодный'],
+                                'охотник': ['убежать', 'голодный'],
+                                'дерево': ['смотреть', 'голодный']
+                            },
+                            'голодный': {
+                                'антилопа': ['съесть', 'сытый'],
+                                'охотник': ['убежать', 'голодный'],
+                                'дерево': ['спать', 'голодный']
+                            }
+                        }
         self.action = ""
 
-    # Реализация методов конечного автомата состояния льва
-    # Сравниваем входной объек и состояние льва с объектами и состояниями списка
-    # Если находим совпадение осуществляем действие и переходим в новое состояние
     def fsm_realisation(self, obj):
-        n = 0
-        while n < len(self.state_list):
-            if obj == self.state_list[n][0] and self.state == self.state_list[n][1]:
-                self.action = self.state_list[n][2]
-                self.state = self.state_list[n][3]
-                print("Исходное_состояние: " + self.state_list[n][1] + " | Объект: " + obj + " | Действие: " + self.action + " | Новое_Состояние :" + self.state)
-                break
-            n += 1
-        if n == len(self.state_list):
-            raise ValueError("Вы ввели неверный объект! Нет такой комбинации состояние + объект")
+        try:
+            action = self.state_list[self.state][obj][0]
+            state = self.state_list[self.state][obj][1]
+        except Exception:
+            raise ValueError("Вы ввели не верный объект! Нет такой комбинации состояние + объект")
+        print("Исходное_состояние: " + self.state + " | Объект: " + obj + " | Действие: " +
+              self.state_list[self.state][obj][0] + " | Новое_Состояние :" + self.state_list[self.state][obj][1])
+        self.action = self.state_list[self.state][obj][0]
+        self.state = self.state_list[self.state][obj][1]
                 
