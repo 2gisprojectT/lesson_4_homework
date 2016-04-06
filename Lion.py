b924ff86cf3720bef4eaef7f1e906f1f6059c2ed
alexey <alexld45@mail.ru>
2016-04-06 12:55:58 +0600

    добавил файлы дз

---
+++ Lion.py
@@ -0,0 +1,31 @@
+class Lion:
+    def __init__(self, state):
+
+        if state.lower() == "сытый" or state.lower() == "голодный":
+            self.state = state.lower()
+        else:
+            raise ValueError("Неверное состояние")
+        self.action = ""
+    def input_object(self, object):
+        if self.state == "сытый":
+            if object.lower() == "антилопа":
+                self.state = "голодный"
+                self.action = "спать"
+            elif object.lower() == "охотник":
+                self.action = "убежать"
+                self.state = "голодный"
+            elif object.lower() == "дерево":
+                self.action = "смотреть"
+                self.state = "голодный"
+            else:
+                raise ValueError("Неверный объект")
+        elif self.state == "голодный":
+            if object.lower() == "антилопа":
+                self.action = "съесть"
+                self.state = "сытый"
+            elif object.lower() == "охотник":
+                self.action = "убежать"
+            elif object.lower() == "дерево":
+                self.action = "спать"
+            else:
+                raise ValueError("Неверный объект")
