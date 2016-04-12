05a8211d229342606e1972af83a2ac71f5c71e49
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-12 13:39:06 +0600

    Update LionStFMS.py
    заменила списки на словари в словаре state_list
    сделала обращения по ключам

--- LionStFMS.py
+++ LionStFMS.py
@@ -7,14 +7,14 @@ class LionStateFSM:
     def __init__(self, state):
         self.state_list = {
                             states[0]: {
-                                        objects[0]: [actions[1], states[1]],
-                                        objects[1]: [actions[2], states[1]],
-                                        objects[2]: [actions[3], states[1]]
+                                        objects[0]: {'action': actions[1], 'state': states[1]},
+                                        objects[1]: {'action': actions[2], 'state': states[1]},
+                                        objects[2]: {'action': actions[3], 'state': states[1]}
                                        },
                             states[1]: {
-                                        objects[0]: [actions[0], states[0]],
-                                        objects[1]: [actions[2], states[1]],
-                                        objects[2]: [actions[1], states[1]]
+                                        objects[0]: {'action': actions[0], 'state': states[0]},
+                                        objects[1]: {'action': actions[2], 'state': states[1]},
+                                        objects[2]: {'action': actions[1], 'state': states[1]}
                                     }
                         }
         if state not in self.state_list:
@@ -25,5 +25,5 @@ class LionStateFSM:
     def fsm_realisation(self, obj):
         if obj not in self.state_list[self.state]:
             raise ValueError("Вы ввели не верный объект! Нет такой комбинации состояние + объект")
-        self.action = self.state_list[self.state][obj][0]
-        self.state = self.state_list[self.state][obj][1]
+        self.action = self.state_list[self.state][obj]['action']
+        self.state = self.state_list[self.state][obj]['state']
