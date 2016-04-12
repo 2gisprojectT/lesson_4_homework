348e847dff1135be25a05f36219b20558638ea26
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-12 12:54:42 +0600

    Update LionSateFMS_Test.py
    Оптимизировала тесты

--- LionSateFMS_Test.py
+++ LionSateFMS_Test.py
@@ -11,68 +11,36 @@ state_list = {
 
 
 class LionSateFMSTest(TestCase):
-    def test_init(self):
+
+    def test_init_state(self):
+        for state in states:
+            lion = LionStateFSM(state)
+            self.assertEqual(state, lion.state, "Ошибка! Должен быть " + state)
+
+    def test_bad_init(self):
         self.assertRaises(ValueError, LionStateFSM, "сонный")
 
-    def test_fms_bad_obj(self):
+    def test_init_obj_at_fed_lion(self):
+        for obj in state_list[states[0]]:
+            lion = LionStateFSM(states[0])
+            lion.fsm_realisation(obj)
+            self.assertEqual(lion.state_list[states[0]][obj][0], lion.action,
+                             "Ошибка! Должен быть " + lion.state_list[states[0]][obj][0])
+            self.assertEqual(lion.state_list[states[0]][obj][1], lion.state,
+                             "Ошибка! Должен быть " + lion.state_list[states[0]][obj][1])
+
+    def test_init_obj_at_hungry_lion(self):
+        for obj in state_list[states[1]]:
+            lion = LionStateFSM(states[1])
+            lion.fsm_realisation(obj)
+            self.assertEqual(lion.state_list[states[1]][obj][0], lion.action,
+                             "Ошибка! Должен быть " + lion.state_list[states[0]][obj][0])
+            self.assertEqual(lion.state_list[states[1]][obj][1], lion.state,
+                             "Ошибка! Должен быть " + lion.state_list[states[0]][obj][1])
+
+    def test_init_bad_obj(self):
         lion = LionStateFSM(states[1])
         self.assertRaises(ValueError, lion.fsm_realisation, "обезьяна")
-
-    def test_init_hungry(self):
-        lion = LionStateFSM(states[1])
-        self.assertEqual(states[1], lion.state, "Ошибка! Должен быть " + states[1])
-
-    def test_init_fed(self):
-        lion = LionStateFSM(states[0])
-        self.assertEqual(states[0], lion.state, "Ошибка! Должен быть" + states[0])
-
-    def test_fms_fed_antelope(self):
-        lion = LionStateFSM(states[0])
-        lion.fsm_realisation(state_list[states[0]][0])
-        self.assertEqual(lion.state_list[states[0]][objects[0]][0], lion.action,
-                         "Ошибка! Должен быть " + lion.state_list[states[0]][objects[0]][0])
-        self.assertEqual(lion.state_list[states[0]][objects[0]][1], lion.state,
-                         "Ошибка! Должен быть " + lion.state_list[states[0]][objects[0]][1])
-
-    def test_fms_hungry_antelope(self):
-        lion = LionStateFSM(states[1])
-        lion.fsm_realisation(state_list[states[1]][0])
-        self.assertEqual(lion.state_list[states[1]][objects[0]][0], lion.action,
-                         "Ошибка! Должен быть " + lion.state_list[states[1]][objects[0]][0])
-        self.assertEqual(lion.state_list[states[1]][objects[0]][1], lion.state,
-                         "Ошибка! Должен быть " + lion.state_list[states[1]][objects[0]][1])
-
-    def test_fms_fed_hunter(self):
-        lion = LionStateFSM(states[0])
-        lion.fsm_realisation(state_list[states[0]][1])
-        self.assertEqual(lion.state_list[states[0]][objects[1]][0], lion.action,
-                         "Ошибка! Должен быть " + lion.state_list[states[0]][objects[1]][0])
-        self.assertEqual(lion.state_list[states[0]][objects[1]][1], lion.state,
-                         "Ошибка! Должен быть " + lion.state_list[states[0]][objects[1]][1])
-
-    def test_fms_hungry_hunter(self):
-        lion = LionStateFSM(states[1])
-        lion.fsm_realisation(state_list[states[1]][1])
-        self.assertEqual(lion.state_list[states[1]][objects[1]][0], lion.action,
-                         "Ошибка! Должен быть " + lion.state_list[states[1]][objects[1]][0])
-        self.assertEqual(lion.state_list[states[1]][objects[1]][1], lion.state,
-                         "Ошибка! Должен быть " + lion.state_list[states[1]][objects[1]][1])
-
-    def test_fms_fed_tree(self):
-        lion = LionStateFSM(states[0])
-        lion.fsm_realisation(state_list[states[0]][2])
-        self.assertEqual(lion.state_list[states[0]][objects[2]][0], lion.action,
-                         "Ошибка! Должен быть " + lion.state_list[states[0]][objects[2]][0])
-        self.assertEqual(lion.state_list[states[0]][objects[2]][1], lion.state,
-                         "Ошибка! Должен быть " + lion.state_list[states[0]][objects[2]][1])
-
-    def test_fms_hungry_tree(self):
-        lion = LionStateFSM(states[1])
-        lion.fsm_realisation(state_list[states[1]][2])
-        self.assertEqual(lion.state_list[states[1]][objects[2]][0], lion.action,
-                         "Ошибка! Должен быть " + lion.state_list[states[1]][objects[2]][0])
-        self.assertEqual(lion.state_list[states[1]][objects[2]][1], lion.state,
-                         "Ошибка! Должен быть " + lion.state_list[states[1]][objects[2]][1])
 
 if __name__ == '__main__':
     unittest.main()
