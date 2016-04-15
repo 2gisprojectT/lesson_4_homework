01351ff84e3c4690a44943dd55a79a4b8a1ff87f
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-15 20:38:02 +0600

    Update LionStFMS.py

--- LionStFMS.py
+++ LionStFMS.py
@@ -2,14 +2,14 @@
 
 class LionStateFSM:
     def __init__(self, state, state_list):
-        if state not in state_list:
+        if state_list['state'] != state:
             raise ValueError("Нет такого состояния: ", state)
         self.state_list = state_list
         self.state = state
         self.action = ""
 
     def fsm_realisation(self, obj):
-        if obj not in self.state_list[self.state]:
+        if self.state_list['object'] != obj:
             raise ValueError("Вы ввели не верный объект! Нет такой комбинации состояние + объект")
-        self.action = self.state_list[self.state][obj]['action']
-        self.state = self.state_list[self.state][obj]['new_state']
+        self.action = self.state_list['action']
+        self.state = self.state_list['new_state']
