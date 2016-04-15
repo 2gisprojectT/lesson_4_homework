d9f2f5ba99d09c19839587e18839afcb1ba9fac3
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-15 16:51:39 +0600

    Update LionStFMS.py
    Сделала список входным параметром при создании Льва

--- LionStFMS.py
+++ LionStFMS.py
@@ -1,19 +1,8 @@
 
 
 class LionStateFSM:
-    def __init__(self, state):
-        self.state_list = {
-                            'сытый': {
-                                        'антилопа': {'action': 'спать', 'new_state': 'голодный'},
-                                        'охотник': {'action': 'убежать', 'new_state': 'голодный'},
-                                        'дерево': {'action': 'смотреть', 'new_state': 'голодный'}
-                                       },
-                            'голодный': {
-                                        'антилопа': {'action': 'съесть', 'new_state': 'голодный'},
-                                        'охотник': {'action': 'убежать', 'new_state': 'голодный'},
-                                        'дерево': {'action': 'спать', 'new_state': 'голодный'}
-                                    }
-                        }
+    def __init__(self, state, state_list):
+        self.state_list = state_list
         if state not in self.state_list:
             raise ValueError("Нет такого состояния: ", state)
         self.state = state
