17a451c4deea86d50efc3e838361374264975cc9
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-19 13:55:05 +0600

    Update LionStFMS.py
    Вернула назад работу со словарем, как было;
    Добавила проверку словаря, что он не пустой. В дальнейшем нужно сделать проверку формата

--- LionStFMS.py
+++ LionStFMS.py
@@ -2,14 +2,16 @@
 
 class LionStateFSM:
     def __init__(self, state, state_list):
-        if state_list['state'] != state:
+        if state_list is None:
+            raise ValueError("Список пуст!")
+        if state not in state_list:
             raise ValueError("Нет такого состояния: ", state)
         self.state_list = state_list
         self.state = state
-        self.action = ""
+        self.action = None
 
     def fsm_realisation(self, obj):
-        if self.state_list['object'] != obj:
+        if obj not in self.state_list[self.state]:
             raise ValueError("Вы ввели не верный объект! Нет такой комбинации состояние + объект")
-        self.action = self.state_list['action']
-        self.state = self.state_list['new_state']
+        self.action = self.state_list[self.state][obj]['action']
+        self.state = self.state_list[self.state][obj]['new_state']
