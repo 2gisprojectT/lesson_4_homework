47a819736878000bc466374b9083e38144517fe4
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-19 13:51:51 +0600

    Update LionSateFMS_Test.py
    Отредактировала согласно замечаниям:
    Вернула словарь, сделала обертку для данных, убрала лишние комментарии, добавила проверки

--- LionSateFMS_Test.py
+++ LionSateFMS_Test.py
@@ -2,28 +2,45 @@ from unittest import TestCase
 from LionStFMS import LionStateFSM
 import unittest
 
-lion_state_list = {'state': 'сытый', 'object': 'антилопа', 'action': 'спать', 'new_state': 'голодный'}
-bad_state_list = {'bad_state': 'сонный', 'bad_object': 'обезьяна'}
+lion_state_list = {
+                     'сытый': {
+                         'антилопа': {'action': 'спать', 'new_state': 'голодный', 'bad_obj': 'обезьяна', 'bad_state': 'сонный'}
+                     }
+                   }
+
+state_list_wrapper = {
+                      'state': tuple(lion_state_list)[0],
+                      'object': tuple(lion_state_list[tuple(lion_state_list)[0]])[0],
+                      'action': lion_state_list[tuple(lion_state_list)[0]][tuple(lion_state_list[tuple(lion_state_list)[0]])[0]]['action'],
+                      'new_state': lion_state_list[tuple(lion_state_list)[0]][tuple(lion_state_list[tuple(lion_state_list)[0]])[0]]['new_state'],
+                      'bad_state': lion_state_list[tuple(lion_state_list)[0]][tuple(lion_state_list[tuple(lion_state_list)[0]])[0]]['bad_state'],
+                      'bad_obj': lion_state_list[tuple(lion_state_list)[0]][tuple(lion_state_list[tuple(lion_state_list)[0]])[0]]['bad_obj']
+                      }
 
 
 class LionSateFMSTest(TestCase):
 
     def test_good_init_state(self):
-        lion = LionStateFSM(lion_state_list['state'], lion_state_list)
-        self.assertEqual(lion_state_list['state'], lion.state)
+        lion = LionStateFSM(state_list_wrapper['state'], lion_state_list)
+        self.assertEqual(state_list_wrapper['state'], lion.state)
+        self.assertEqual(None, lion.action)
 
     def test_bad_init_state(self):
-        self.assertRaises(ValueError, LionStateFSM, bad_state_list['bad_state'], lion_state_list)
+        self.assertRaises(ValueError, LionStateFSM, state_list_wrapper['bad_state'], lion_state_list)
+
+    def test_empty_data(self):
+        self.assertRaises(ValueError, LionStateFSM, state_list_wrapper[None], None)
 
     def test_init_good_obj(self):
-        lion = LionStateFSM(lion_state_list['state'], lion_state_list)
-        lion.fsm_realisation(lion_state_list['object'])
-        self.assertEqual(lion_state_list['action'], lion.action)
-        self.assertEqual(lion_state_list['new_state'], lion.state)
+        lion = LionStateFSM(state_list_wrapper['state'], lion_state_list)
+        lion.fsm_realisation(state_list_wrapper['object'])
+        self.assertEqual(state_list_wrapper['action'], lion.action)
+        self.assertEqual(state_list_wrapper['new_state'], lion.state)
 
     def test_init_bad_obj(self):
-        lion = LionStateFSM(lion_state_list['state'], lion_state_list)
-        self.assertRaises(ValueError, lion.fsm_realisation, bad_state_list['bad_object'])
+        lion = LionStateFSM(state_list_wrapper['state'], lion_state_list)
+        self.assertRaises(ValueError, lion.fsm_realisation, state_list_wrapper['bad_obj'])
 
 if __name__ == '__main__':
     unittest.main()
+    
