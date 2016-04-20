e56f966d3a261496c8495f5d6200cc4c0bb117c7
AntoninaFantalina <fantalinaa@mail.ru>
2016-04-20 16:07:33 +0600

    Изменение "LionTest.py" Устранение "дикости" в тестах, добавление вспомогательных переменных

--- LionTest.py
+++ LionTest.py
@@ -3,27 +3,27 @@ from Lion import Lion
 import unittest
 
 trans_table = {'сытый': {'антилопа': {'action': 'спать', 'state': 'голодный'}, }, }
+key_state = list(trans_table.keys())[0]
+key_symbol = list(trans_table[key_state].keys())[0]
 
 
 class LionUnittest(TestCase):
     def test_init(self):
-        lion = Lion(list(trans_table.keys())[0], trans_table)
-        self.assertEqual(list(lion.trans_table.keys())[0], lion.state)
+        lion = Lion(key_state, trans_table)
+        self.assertEqual(key_state, lion.state)
         self.assertEqual(trans_table, lion.trans_table)
 
     def test_init_negative(self):
         self.assertRaises(ValueError, Lion, "уставший", trans_table)
 
     def test_transition(self):
-        lion = Lion(list(trans_table.keys())[0], trans_table)
-        lion.transition(list(lion.trans_table[list(lion.trans_table.keys())[0]].keys())[0])
-        self.assertEqual(lion.trans_table[list(lion.trans_table.keys())[0]]
-                         [list(lion.trans_table[list(lion.trans_table.keys())[0]].keys())[0]]['action'], lion.action)
-        self.assertEqual(lion.trans_table[list(lion.trans_table.keys())[0]]
-                         [list(lion.trans_table[list(lion.trans_table.keys())[0]].keys())[0]]['state'], lion.state)
+        lion = Lion(key_state, trans_table)
+        lion.transition(key_symbol)
+        self.assertEqual(trans_table[key_state][key_symbol]['action'], lion.action)
+        self.assertEqual(trans_table[key_state][key_symbol]['state'], lion.state)
 
     def test_transition_negative(self):
-        lion = Lion(list(trans_table.keys())[0], trans_table)
+        lion = Lion(key_state, trans_table)
         self.assertRaises(ValueError, lion.transition, "НЛО")
 
 
