f5a5088bfb4371149c2508fe8064aeff940c1b06
Alexander Zolotarev <kot_optimist@mail.ru>
2016-04-24 20:19:44 +0600

    Добавлены измененные файлы ДЗ

--- LionTest.py
+++ LionTest.py
@@ -5,44 +5,43 @@ import unittest
 
 class LionUnittest(TestCase):
     def test_init(self):
-        self.assertRaises(ValueError, Lion, "не сытый")
-        self.assertRaises(ValueError, Lion, "не очень голодный")
+        self.assertRaises(ValueError, Lion, "ошибочное значение")
 
         lion = Lion("сытый")
-        self.assertEqual("сытый", lion.state, ValueError)
+        self.assertEqual("сытый", lion.state)
         lion = Lion("голодный")
-        self.assertEqual("голодный", lion.state, ValueError)
+        self.assertEqual("голодный", lion.state)
 
     def test_act(self):
         lion = Lion("сытый")
         lion.act("антилопа")
-        self.assertEqual("голодный", lion.state, ValueError)
-        self.assertEqual("спать", lion.action, ValueError)
+        self.assertEqual("голодный", lion.state)
+        self.assertEqual("спать", lion.action)
 
         lion = Lion("сытый")
         lion.act("охотник")
-        self.assertEqual("голодный", lion.state, ValueError)
-        self.assertEqual("убежать", lion.action, ValueError)
+        self.assertEqual("голодный", lion.state)
+        self.assertEqual("убежать", lion.action)
 
         lion = Lion("сытый")
         lion.act("дерево")
-        self.assertEqual("голодный", lion.state, ValueError)
-        self.assertEqual("смотреть", lion.action, ValueError)
+        self.assertEqual("голодный", lion.state)
+        self.assertEqual("смотреть", lion.action)
 
         lion = Lion("голодный")
         lion.act("антилопа")
-        self.assertEqual("сытый", lion.state, ValueError)
-        self.assertEqual("съесть", lion.action, ValueError)
+        self.assertEqual("сытый", lion.state)
+        self.assertEqual("съесть", lion.action)
 
         lion = Lion("голодный")
         lion.act("охотник")
-        self.assertEqual("голодный", lion.state, ValueError)
-        self.assertEqual("убежать", lion.action, ValueError)
+        self.assertEqual("голодный", lion.state)
+        self.assertEqual("убежать", lion.action)
 
         lion = Lion("голодный")
-        lion.act("дерево")
-        self.assertEqual("голодный", lion.state, ValueError)
-        self.assertEqual("спать", lion.action, ValueError)
+        lion.act("деревоi")
+        self.assertEqual("голодный", lion.state)
+        self.assertEqual("спать", lion.action)
 
 
 if __name__ == '__main__':
