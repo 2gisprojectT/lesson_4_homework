5373f701fefa2ba687436b7b1ef5d82cc073807b
Andrey Kargapolov <bilininandroid@gmail.com>
2016-04-28 00:34:20 +0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,12 +7,12 @@ from main import lion
 class LionTests(TestCase):
     def test_all_dictionary(self):
         dictionary = lion
-        for state in dictionary.keys():
-            for symbol in dictionary[state].keys():
+        for state in dictionary["state"].keys():
+            for symbol in dictionary["state"][state]["symbol"].keys():
                 lion_ = FSM(dictionary, state)
                 lion_.input(symbol)
-                self.assertEqual(lion_.action, dictionary[state][symbol]['action'])
-                self.assertEqual(lion_.state, dictionary[state][symbol]['state'])
+                self.assertEqual(lion_.action, dictionary["state"][state]["symbol"][symbol]['action'])
+                self.assertEqual(lion_.state, dictionary["state"][state]["symbol"][symbol]['state'])
 
 
 if __name__ == '__main__':
