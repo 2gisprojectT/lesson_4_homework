32ce85025b9d53d793207d51c94bfe5d65280796
Andrey Kargapolov <bilininandroid@gmail.com>
2016-04-28 00:31:52 +0700

    переделал структуру словаря
    На мой взгляд сделал структуру словаря понятнее, чтобы вытащить состояния теперь dictionary["state"].keys() вместо непонятного dictionary.keys() и т.д.

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-"""
-Структура словаря имеет вид:
-    {<состояние>:<символ>:{"action":<действие>,"state":<новое состояние>}
-"""
-lion = {"голодный":
-            {"антилопа": {"action": "съесть", "state": "сытый"},
-             "охотник": {"action": "убежать", "state": "голодный"},
-             "дерево": {"action": "спать", "state": "голодный"}},
-        "сытый":
-            {"антилопа": {"action": "спать", "state": "голодный"},
-             "охотник": {"action": "убежать", "state": "голодный"},
-             "дерево": {"action": "смотреть", "state": "голодный"}}}
+lion = {"state": {"голодный": {"symbol":
+                                   {"антилопа": {"action": "съесть", "state": "сытый"},
+                                    "охотник": {"action": "убежать", "state": "голодный"},
+                                    "дерево": {"action": "спать", "state": "голодный"}}}},
+        "state": {"сытый": {"symbol":
+                                {"антилопа": {"action": "спать", "state": "голодный"},
+                                 "охотник": {"action": "убежать", "state": "голодный"},
+                                 "дерево": {"action": "смотреть", "state": "голодный"}}}}}
 
 
 class FSM:
     def __init__(self, dictionary, state):
-        if not state in dictionary.keys():
+        if not state in dictionary["state"].keys():
             raise ValueError("Состояния нет в словаре переходов")
         else:
             self.dictionary = dictionary
@@ -22,8 +18,8 @@ class FSM:
             self.action = ""
 
     def input(self, symbol):
-        if not symbol in self.dictionary[self.state].keys():
+        if not symbol in self.dictionary["state"][self.state]["symbol"].keys():
             raise ValueError('Входного символа нет в словаре переходов')
         else:
-            self.action = self.dictionary[self.state][symbol]['action']
-            self.state = self.dictionary[self.state][symbol]['state']
+            self.action = self.dictionary["state"][self.state]["symbol"][symbol]['action']
+            self.state = self.dictionary["state"][self.state]["symbol"][symbol]['state']
