5a93cd6acb789c5803714893a56cf28b0617b7e6
Andrey Kargapolov <bilininandroid@gmail.com>
2016-04-28 15:03:26 +0600

    убрал лишние else

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ class FSM:
     def __init__(self, dictionary, state):
         if not state in dictionary.keys():
             raise ValueError("Состояния нет в словаре переходов")
-        else:
-            self.dictionary = dictionary
-            self.state = state
-            self.action = None
+        self.dictionary = dictionary
+        self.state = state
+        self.action = None
 
     def input(self, symbol):
         if not symbol in self.dictionary[self.state].keys():
             raise ValueError('Входного символа нет в словаре переходов')
-        else:
-            self.action = self.dictionary[self.state][symbol]['action']
-            self.state = self.dictionary[self.state][symbol]['state']
+        self.action = self.dictionary[self.state][symbol]['action']
+        self.state = self.dictionary[self.state][symbol]['state']
