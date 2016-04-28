9bf0b4b4c2bca685fa7ce4eeba2c892308d1ec5c
Andrey Kargapolov <bilininandroid@gmail.com>
2016-04-28 12:52:09 +0700

    Переместил таблицу сюда, переписал тесты.

--- tests.py
+++ tests.py
@@ -1,18 +1,41 @@
 from unittest import TestCase
 import unittest
 from main import FSM
-from main import lion
+
+lion = {"голодный": {"антилопа": {"action": "съесть", "state": "сытый"},
+                     "охотник": {"action": "убежать", "state": "голодный"},
+                     "дерево": {"action": "спать", "state": "голодный"}},
+        "сытый": {"антилопа": {"action": "спать", "state": "голодный"},
+                  "охотник": {"action": "убежать", "state": "голодный"},
+                  "дерево": {"action": "смотреть", "state": "голодный"}}}
 
 
 class LionTests(TestCase):
-    def test_all_dictionary(self):
+    def test_good_init(self):
         dictionary = lion
-        for state in dictionary["state"].keys():
-            for symbol in dictionary["state"][state]["symbol"].keys():
-                lion_ = FSM(dictionary, state)
-                lion_.input(symbol)
-                self.assertEqual(lion_.action, dictionary["state"][state]["symbol"][symbol]['action'])
-                self.assertEqual(lion_.state, dictionary["state"][state]["symbol"][symbol]['state'])
+        state = list(dictionary.keys())[0]
+        symbol = list(dictionary[state].keys())[0]
+        lion_ = FSM(dictionary, state)
+        self.assertEqual(lion_.action, None)
+        self.assertEqual(lion_.state, state)
+
+    def test_bad_init(self):
+        self.assertRaises(ValueError, FSM, lion, "холодный")
+
+    def test_bad_input(self):
+        dictionary = lion
+        state = list(dictionary.keys())[0]
+        lion_ = FSM(dictionary, state)
+        self.assertRaises(ValueError, lion_.input, "речка")
+
+    def test_good_input(self):
+        dictionary = lion
+        state = list(dictionary.keys())[0]
+        symbol = list(dictionary[state].keys())[0]
+        lion_ = FSM(dictionary, state)
+        lion_.input(symbol)
+        self.assertEqual(lion_.action, lion[state][symbol]["action"])
+        self.assertEqual(lion_.state, lion[state][symbol]["state"])
 
 
 if __name__ == '__main__':
